6430c75f88197ced47d222c884e1d3693ea9d6ac
aLe-kLaus <alessomklaus13@gmail.com>
2021-01-07 11:42:06 -0300

    navbar finished, but without the searchbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import logoImage from '../../assets/logo.png';
-import './index.css';
+import './styles.css';
 import {menuItems} from './menuItems';
+// import { Button } from '../Searchbar';
 
 class Navbar extends Component {
+  state = { clicked: false }
+
+  userClick = () => {
+    this.setState({ clicked: !this.state.clicked})
+  };
+
   render() {
     return(
       <nav className="nav-items">
-        <img src={logoImage} className="nav-logo"/>
-        <div className="menu-icon">
-
+        <img alt='' src={logoImage} className="nav-logo"/>
+        <div className="menu-icon" onClick={this.userClick}>
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
-        <ul>
-          {menuItems.map((item, index) => {
-            return (
-              <li>
+        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+          {menuItems.map((item, index) => (
+              <li key={item.id}>
                 <a className={item.cName} href={item.url}>
                   {item.title}
                 </a>
               </li>
             )
-          })}
+          )}
         </ul>
+        <input placeholder="search for your favorite movie"></input>
+        <button>Search</button>
       </nav>
     )
   };
--- src/components/Navbar/menuItems.js
+++ src/components/Navbar/menuItems.js
@@ -1,17 +1,14 @@
 export const menuItems = [
   {
+    id: 1,
     title: 'Trending',
     url: '/',
     cName: 'nav-links'
   },
   {
+    id: 2,
     title: 'Repository',
     url: '/repository',
     cName: 'nav-links'
-  },
-  {
-    title: 'React',
-    url: 'localhost:3000',
-    cName: 'nav-links-mobile'
   }
 ]
